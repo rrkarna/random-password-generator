e9c8d688222e6048303b1950b07514ea3ebcf1ee
rrkarna <145824398+rrkarna@users.noreply.github.com>
2023-10-24 03:43:33 +0530

    Add files via upload

---
+++ src/characters.js
@@ -0,0 +1,7 @@
+export const numbers = '0123456789'
+
+export const upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+export const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+
+export const specialCharacters = "!'^+%&/()=?_#$%${[]}|;:>÷`<.*-@é"
